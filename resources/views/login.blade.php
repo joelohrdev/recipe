cf17ffe430e16195b3b93483086d23fdbf5b7726
jlarm <117611704+jlarm@users.noreply.github.com>
2026-01-07 15:13:53 -0600

    Added logo

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,3 +1,6 @@
 <x-layout>
-    <livewire:login />
+    <div class="flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
+        <img class="w-36 h-auto mb-5" src="{{ asset('logo-s.webp') }}" alt="">
+        <livewire:login />
+    </div>
 </x-layout>
